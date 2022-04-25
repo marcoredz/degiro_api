d9b7f2fc3aa3c8d6372e7e9c1a8a2ac528d5ba63
Marco Rossi <marcoreds9724@gmail.com>
2022-04-25 16:19:30 +0200

    exporting abstracts

--- lib/degiro_api.dart
+++ lib/degiro_api.dart
@@ -4,5 +4,6 @@
 library degiro_api;
 
 export 'src/degiro_api_base.dart' show DegiroApi;
+export 'src/config/abstracts.dart';
 export 'src/config/degiro_api_error.dart';
 export 'src/data/models/public/publics.dart';
